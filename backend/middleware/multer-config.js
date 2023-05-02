42c82c5d85329d64312f209065e2d45fe2b1d0d5
IALTAIR <yferoze@gmail.com>
2023-05-02 21:06:13 +0200

    first commit

---
+++ backend/middleware/multer-config.js
@@ -0,0 +1,30 @@
+// Package multer
+const multer = require("multer");
+
+// Types media
+const MIME_TYPES = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
+  "image/bmp": "bmp",
+  "image/gif": "gif",
+  "image/x-icon": "ico",
+  "image/svg+xml": "svg",
+  "image/tiff": "tiff",
+  "image/tif": "tif",
+  "image/webp": "webp",
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(" ").join("_");
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + "." + extension);
+  },
+});
+
+// Export multer
+module.exports = multer({ storage: storage }).single("image");
